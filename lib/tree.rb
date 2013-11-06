f7ea553324cf4d776406bb95fd66d805de5a059d
Thomas Kerber <t.kerber@online.de>
2013-11-06 20:49:23 +0000

    UI implementation + documenting

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,17 +2,29 @@ require_relative 'crypto'
 
 module PasswdManage
   
+  # A directory-esque tree with labeled nodes and edges.
   class Tree
     
     include Enumerable
     
+    
     attr_accessor :val
     
+    # Creates a new Tree.
+    # 
+    # @param val [Object] The node label of the tree.
     def initialize(val = nil)
       @val = val
       @succ = {}
     end
     
+    # Gets a subtree by the label of the edge leading to it.
+    # 
+    # @param key [String] The edge label.
+    # @param create [Boolean] Whether or not the create a new Node if there
+    #   is no edge with the label given.
+    # @return [Tree, nil] The tree at the edge, or nil if it didn't exist
+    #   annd create was false.
     def [](key, create = false)
       if create and not @succ.include? key
         @succ[key] = Tree.new
@@ -21,37 +33,64 @@ module PasswdManage
       end
     end
     
+    # Iterates over (edge, node) pairs.
+    # 
+    # @yield [key, tree] Edge, node pairs of connected nodes.
     def each
       @succ.each do |i|
         yield i
       end
     end
     
-    # key is a /-seperated string of keys to use.
+    # Gets a (more distant descendant of the current node.
+    # 
+    # @key [String] A forward-slash seperated list of the edge labels to
+    #   follow.
+    # @create [Boolean] Whether or not to create nodes if the edge labels
+    #   aren't used yet.
+    # @return [Tree, nil] The node connected through the edge labels, or nil
+    #   if there is no such node and create was false.
     def descendant(key, create = false)
       if key.include? '/'
         key, keys = key.split('/', 2)
-        self[key, create].descendant(keys, create)
+        child = self[key, create]
+        if child
+          child.descendant(keys, create)
+        end
       else
         self[key, create]
       end
     end
     
-    def to_s(key, indent = 0)
-      s= ''
-      s += Crypto.decrypt(key, val) if val
-      s += "\n"
+    # Prints the skeleton of the tree. Node labels are NOT printed.
+    # 
+    # @param indent [Int] By how many stages to indent the tree.
+    # @return [String] The skeleton of the tree.
+    def to_s(indent = 0)
+      s = ""
       each do |k, v|
-        s += ' ' * (indent + 2) + k + ':' + v.to_s(key, indent + 2)
+        s += '  ' * (indent) + k + "/\n" + v.to_s(indent + 1)
       end
-      s
+      return s
     end
     
   end
   
   
+  # A tree loaded from an encrypted file.
+  # 
+  # All values are themselves encrypted with the key again. This doesn't
+  # add additional security, but prevents them from appearing in memory in
+  # plaintext if avoidable. Note that any program designed specifically
+  # to tap into this programs memory will have no problem with this.
   class EncryptedTree < EncryptedFile
     
+    # Creates a new tree / load an existing one from a file.
+    # 
+    # @param passwd [String] The password to decrypt the file. Discarded
+    #   after this call (although the key is kept in memory)
+    # @param file [String] The location of the file to decrypt. If this does
+    #   not exist, a new tree is made.
     def initialize(passwd, file)
       super(passwd, file)
       if @cont == nil
@@ -59,20 +98,30 @@ module PasswdManage
       end
     end
     
-    # All values are themselves encrypted with the key again. This doesn't
-    # add additional security, but prevents them from appearing in memory in
-    # plaintext if avoidable. Note that any program designed specifically
-    # to tap into this programs memory will have no problem with this.
+    # Retrieves a node label following a forward-slash seperated list of
+    # edge labels.
+    # 
+    # @param key [String] The forward-slash seperated list of edge labels.
+    # @return [String] The decrypted value of the corresponding node label.
     def [](key)
       Crypto.decrypt(@key, @cont.descendant(key).val)
     end
     
+    # Sets a node label corresponding to a forward-slash seperated list of
+    # edge labels.
+    # 
+    # @param key [String] The list of edge labels.
+    # @param val [String] The value to set the node label to. This will be
+    #   encrypted.
     def []=(key, val)
       @cont.descendant(key, true).val = Crypto.encrypt(@key, val)
     end
     
+    # Delegates to the tree for string representation.
+    # 
+    # @return [String] The string representation of the tree.
     def to_s
-      @cont.to_s(@key)
+      @cont.to_s
     end
     
   end
--- lib/ui.rb
+++ lib/ui.rb
@@ -1,16 +1,20 @@
 require 'highline/import'
+require 'readline'
 
 require_relative 'tree'
 
 module PasswdManage
   
-  VERSION = 0.0.0
+  VERSION = "0.0.0"
   
   module UI
     
     DEFAULT_FILE = '~/.passwd_manage/default.enc'
     PROMPT = '> '
     
+    # Loads and decrypts a file. The password is asked for interactively.
+    # 
+    # @param file [String] The file to load from.
     def self.load_file(file = DEFAULT_FILE)
       $file = file
       until $tree
@@ -29,29 +33,132 @@ module PasswdManage
       end
     end
     
+    # Closes the tree. This generally only happens right before ruby is about
+    # to exit so it isn't that important but hey.
     def self.close_file
-      $file.close
-      $file = nil
+      $tree.close
+      $tree = nil
     end
     
+    # Runs the program.
+    def self.main
+      # TODO: config, by argv and possibly by config file.
+      load_file
+      welcome
+      begin
+        prompt
+      ensure
+        close_file
+      end
+    end
+    
+    # Prints help text.
+    def self.help
+      puts (
+        "help\t\t - Prints this help text\n"\
+        "set KEY\t\t - Sets the value of the key to a value entered by the "\
+          "user.\n"\
+        "print\t\t - Prins a representation of the tree, without values.\n"\
+        "print KEY\t - Prints the value of the key.\n"\
+        "copy KEY\t - Copies the value of the key.\n"\
+        "copyc INT KEY\t - Copies the (1-indexed) character from the value "\
+          "of the key.\n"\
+        "del KEY\t\t - Deletes the key from the tree. If it has subtrees, "\
+          "the subtrees get deleted if and only if the key had no value.\n"\
+        "copy_junk\t - Erases the system clipboard.\n"\
+        "exit\t\t - Exit.\n\n"\
+        "Keys are sorted in forward-slash seperated tree structure. "\
+        "(slightly remenicient of urls).")
+    end
+    
+    # Prints a value
+    # 
+    # @param key [String] The key for which to retrieve a value.
+    def self.print_val(key)
+      begin
+        tmp_print $tree[key]
+      # No method error arises from trying to work on a nil tree (or trying to
+      # decrypt a nil value).
+      rescue NoMethodError
+        puts "No value entered for key '#{key}'"
+      end
+    end
+    
+    # Prints the currently loaded tree (without values).
+    def self.print_tree
+      puts $tree
+    end
+    
+    private
+    
+    # Prints strings, waits for enter then replaces them. Also adds color
+    # for fancyness.
+    def self.tmp_print(str)
+      # TODO
+      puts str
+    end
+    
+    # Displays welcome text.
     def self.welcome
       puts "passwd_manage version #{VERSION}"
       puts "Using password file #{$file}."
       puts "Welcome to passwd_manage! Type 'help' for a list of commands."
     end
     
-    def self.prompt
-      # TODO implement prompt.
+    # Runs a single command by the user. Also catches most errors and prints
+    # them.
+    def self.run(command)
+      # Ctrl-D will return nil; this should be a quit signal.
+      return :quit unless command
+      # Ignore empty commands
+      return if command == ''
+      command, args = command.split(nil, 2)
+      begin
+        # TODO implement all of these.
+        case command.downcase
+        when 'help'
+          help
+        when 'set'
+          set args
+        when 'print'
+          if args
+            print_val args
+          else
+            print_tree
+          end
+        when 'copy'
+          copy args
+        when 'copyc'
+          copyc_raw args
+        when 'del'
+          del args
+        when 'copy_junk'
+          copy_junk
+        when 'exit'
+          return :quit
+        else
+          puts "Command '#{command}' undefined. Type 'help' for a list of "\
+            "commands."
+        end
+      rescue
+        puts $!
+      end
     end
     
-    def self.main(*argv)
-      # TODO: config, by argv and possibly by config file.
-      load_file
-      welcome
-      prompt
-      close_file
+    # Runs a basic prompt for the user to interface with the program.
+    def self.prompt
+      prompt_hist = []
+      quit = false
+      until quit
+        input = Readline.readline(PROMPT, true)
+        quit = run(input) == :quit
+      end
     end
     
   end
   
+end
+
+if __FILE__ == $0
+  PasswdManage::UI.main
 end
